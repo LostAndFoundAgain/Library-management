b81f08cf116091243c6e28f047eee23f6f2a5d3e
Nikhil Upadhyay <nikrkt1.com>
2025-03-03 20:58:56 +0530

    feature-2-add-member-and-admin-behaviour

--- src/User/Admin.java
+++ src/User/Admin.java
@@ -1,8 +1,35 @@
+//    🔑 Admin Operations
+//    ✅ 1. Add Item → Add a new Book, DVD, or Magazine.
+//    ✅ 2. Remove Item → Delete an item by title.
+//    ✅ 3. Update Item → Modify an item's details (title, author, publication date, etc.).
+//    ✅ 4. View All Items → See all books, DVDs, and magazines in the library.
+//    ✅ 5. Exit to Main Menu → Return to the main selection screen.
+
+
+
 package User;
+
+import Item.Item;
 
 public class Admin extends User {
 
     public Admin(String name) {
         super(name);
+    }
+
+    public void addItem(Item item) {
+
+    }
+
+    public void removeItem(String title) {
+
+    }
+
+    public void updateItem() {
+
+    }
+
+    public void viewItems() {
+
     }
 }
