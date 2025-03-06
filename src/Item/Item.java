6f69e99734594ac9bc2bc51abc4ffd7aafe3b35c
Nikhil Upadhyay <nikrkt1.com>
2025-03-06 09:53:19 +0530

    feature-2-add-uniqueid-item

--- src/Item/Item.java
+++ src/Item/Item.java
@@ -1,12 +1,16 @@
 package Item;
 
+import java.util.UUID;
+
 public abstract class Item {
+    private final UUID itemId;
     private final String name;
     private final String author;
     private final String publicationDate;
     private boolean borrowed;
 
     public Item(String name, String publicationDate, String author) {
+        this.itemId = new UUID(1, 10);
         this.name = name;
         this.publicationDate = publicationDate;
         this.author = author;
